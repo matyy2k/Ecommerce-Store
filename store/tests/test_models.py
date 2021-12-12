a5fc12a466ffb80541b840183dd1a918f22e1b5e
Mateusz M <roadtosilver5@gmail.com>
2021-12-12 14:21:44 +0100

    First views and templates

--- store/tests/test_models.py
+++ store/tests/test_models.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from store.models import Category, Product
 from django.contrib.auth.models import User
 
+
 class TestCategoriesModel(TestCase):
 
     def setUp(self):
